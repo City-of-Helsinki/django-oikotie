6260ed01d49b9e2bffd0497e6ec9af365e41dc50
Eetu Häivälä <eetu.haivala@anders.com>
2020-10-23 10:24:29 +0300

    Test requirements and settings
    Add test requirements to requirements-test files. Add minimal django
    settings module for testing.
    
    Refs ASU-388

---
+++ tests/settings.py
@@ -0,0 +1,24 @@
+DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": ":memory:"}}
+
+INSTALLED_APPS = (
+    "django.contrib.contenttypes",
+    "django.contrib.auth",
+    "django.contrib.sites",
+    "django.contrib.admin",
+    "django.contrib.sessions",
+    "django_oikotie",
+)
+
+MIDDLEWARE = [
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+]
+
+SITE_ID = 1
+DEBUG = True
+USE_TZ = True
+SECRET_KEY = "xxx"
+LANGUAGES = (("fi", "Finnish"), ("en", "English"), ("sv", "Swedish"))
