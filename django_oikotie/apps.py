087eaf65f2e0333abb06028acbe226009360943f
Frank Wickström <frank.wickstrom@anders.fi>
2021-08-31 13:58:01 +0300

    Set the default auto field to BigAutoField
    While not having any models present at the moment, this sets things up the same way
    as Django would do if the application were created with Django 3.2 and would work
    without breaking on Django 4.0

--- django_oikotie/apps.py
+++ django_oikotie/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class DjangoOikotieConfig(AppConfig):
     name = "django_oikotie"
     verbose_name = "Oikotie"
+    default_auto_field = 'django.db.models.BigAutoField'
